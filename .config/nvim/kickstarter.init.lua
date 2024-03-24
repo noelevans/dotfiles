8642feac849e18fafa8a93b3b869365783dd3e41
noelevans <noelevans@gmail.com>
2024-03-24 21:44:05 +0000

    No telescope. Closer to normal basics

--- .config/nvim/kickstarter.init.lua
+++ .config/nvim/kickstarter.init.lua
@@ -1,106 +1,3 @@
---[[
-
-:command! -nargs=1 AG GrepperAg <args>
-:command! -nargs=1 RG GrepperRg <args>
-map Q <Nop>
-nnoremap <leader>h :nohlsearch<cr>
-nnoremap <leader>gg :GrepperRg <C-R><C-W>
-nmap <leader>gh :diffget //3<CR>
-nmap <leader>gu :diffget //2<CR>
-nmap <leader>cn :cnext<CR>
-nnoremap <C-l> <C-i>
-tnoremap <Esc> <C-\><C-n>
-tnoremap <expr> <C-R> '<C-\><C-N>"'.nr2char(getchar()).'pi'   " Overriding Bash's Ctrl-R reverse search
-iabbrev pdb breakpoint()
-let g:fzf_layout = {'down':  '40%'}
-let g:airline#extensions#tagbar#enabled = 1
-let g:airline#extensions#tagbar#flags = 'f'
-ctags
-
-=====================================================================
-==================== READ THIS BEFORE CONTINUING ====================
-=====================================================================
-========                                    .-----.          ========
-========         .----------------------.   | === |          ========
-========         |.-""""""""""""""""""-.|   |-----|          ========
-========         ||                    ||   | === |          ========
-========         ||   KICKSTART.NVIM   ||   |-----|          ========
-========         ||                    ||   | === |          ========
-========         ||                    ||   |-----|          ========
-========         ||:Tutor              ||   |:::::|          ========
-========         |'-..................-'|   |____o|          ========
-========         `"")----------------(""`   ___________      ========
-========        /::::::::::|  |::::::::::\  \ no mouse \     ========
-========       /:::========|  |==hjkl==:::\  \ required \    ========
-========      '""""""""""""'  '""""""""""""'  '""""""""""'   ========
-========                                                     ========
-=====================================================================
-=====================================================================
-
-What is Kickstart?
-
-  Kickstart.nvim is *not* a distribution.
-
-  Kickstart.nvim is a starting point for your own configuration.
-    The goal is that you can read every line of code, top-to-bottom, understand
-    what your configuration is doing, and modify it to suit your needs.
-
-    Once you've done that, you can start exploring, configuring and tinkering to
-    make Neovim your own! That might mean leaving Kickstart just the way it is for a while
-    or immediately breaking it into modular pieces. It's up to you!
-
-    If you don't know anything about Lua, I recommend taking some time to read through
-    a guide. One possible example which will only take 10-15 minutes:
-      - https://learnxinyminutes.com/docs/lua/
-
-    After understanding a bit more about Lua, you can use `:help lua-guide` as a
-    reference for how Neovim integrates Lua.
-    - :help lua-guide
-    - (or HTML version): https://neovim.io/doc/user/lua-guide.html
-
-Kickstart Guide:
-
-  TODO: The very first thing you should do is to run the command `:Tutor` in Neovim.
-
-    If you don't know what this means, type the following:
-      - <escape key>
-      - :
-      - Tutor
-      - <enter key>
-
-    (If you already know the Neovim basics, you can skip this step.)
-
-  Once you've completed that, you can continue working through **AND READING** the rest
-  of the kickstart init.lua.
-
-  Next, run AND READ `:help`.
-    This will open up a help window with some basic information
-    about reading, navigating and searching the builtin help documentation.
-
-    This should be the first place you go to look when you're stuck or confused
-    with something. It's one of my favorite Neovim features.
-
-    MOST IMPORTANTLY, we provide a keymap "<space>sh" to [s]earch the [h]elp documentation,
-    which is very useful when you're not exactly sure of what you're looking for.
-
-  I have left several `:help X` comments throughout the init.lua
-    These are hints about where to find more information about the relevant settings,
-    plugins or Neovim features used in Kickstart.
-
-   NOTE: Look for lines like this
-
-    Throughout the file. These are for you, the reader, to help you understand what is happening.
-    Feel free to delete them once you know what you're doing, but they should serve as a guide
-    for when you are first encountering a few different constructs in your Neovim config.
-
-If you experience any errors while trying to install kickstart, run `:checkhealth` for more info.
-
-I hope you enjoy your Neovim journey,
-- TJ
-
-P.S. You can delete this when you're done too. It's your config now! :)
---]]
-
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
@@ -321,110 +218,110 @@ require("lazy").setup({
 	--
 	-- Use the `dependencies` key to specify the dependencies of a particular plugin
 
-	{ -- Fuzzy Finder (files, lsp, etc)
-		"nvim-telescope/telescope.nvim",
-		event = "VimEnter",
-		branch = "0.1.x",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			{ -- If encountering errors, see telescope-fzf-native README for installation instructions
-				"nvim-telescope/telescope-fzf-native.nvim",
-
-				-- `build` is used to run some command when the plugin is installed/updated.
-				-- This is only run then, not every time Neovim starts up.
-				build = "make",
-
-				-- `cond` is a condition used to determine whether this plugin should be
-				-- installed and loaded.
-				cond = function()
-					return vim.fn.executable("make") == 1
-				end,
-			},
-			{ "nvim-telescope/telescope-ui-select.nvim" },
-
-			-- Useful for getting pretty icons, but requires a Nerd Font.
-			{ "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font },
-		},
-		config = function()
-			-- Telescope is a fuzzy finder that comes with a lot of different things that
-			-- it can fuzzy find! It's more than just a "file finder", it can search
-			-- many different aspects of Neovim, your workspace, LSP, and more!
-			--
-			-- The easiest way to use Telescope, is to start by doing something like:
-			--  :Telescope help_tags
-			--
-			-- After running this command, a window will open up and you're able to
-			-- type in the prompt window. You'll see a list of `help_tags` options and
-			-- a corresponding preview of the help.
-			--
-			-- Two important keymaps to use while in Telescope are:
-			--  - Insert mode: <c-/>
-			--  - Normal mode: ?
-			--
-			-- This opens a window that shows you all of the keymaps for the current
-			-- Telescope picker. This is really useful to discover what Telescope can
-			-- do as well as how to actually do it!
-
-			-- [[ Configure Telescope ]]
-			-- See `:help telescope` and `:help telescope.setup()`
-			require("telescope").setup({
-				-- You can put your default mappings / updates / etc. in here
-				--  All the info you're looking for is in `:help telescope.setup()`
-				--
-				-- defaults = {
-				--   mappings = {
-				--     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-				--   },
-				-- },
-				-- pickers = {}
-				extensions = {
-					["ui-select"] = {
-						require("telescope.themes").get_dropdown(),
-					},
-				},
-			})
-
-			-- Enable Telescope extensions if they are installed
-			pcall(require("telescope").load_extension, "fzf")
-			pcall(require("telescope").load_extension, "ui-select")
-
-			-- See `:help telescope.builtin`
-			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
-			vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
-			vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
-			vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
-			vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
-			vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
-			vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
-			vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
-			vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-			vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
-
-			-- Slightly advanced example of overriding default behavior and theme
-			vim.keymap.set("n", "<leader>/", function()
-				-- You can pass additional configuration to Telescope to change the theme, layout, etc.
-				builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
-					winblend = 10,
-					previewer = false,
-				}))
-			end, { desc = "[/] Fuzzily search in current buffer" })
-
-			-- It's also possible to pass additional configuration options.
-			--  See `:help telescope.builtin.live_grep()` for information about particular keys
-			vim.keymap.set("n", "<leader>s/", function()
-				builtin.live_grep({
-					grep_open_files = true,
-					prompt_title = "Live Grep in Open Files",
-				})
-			end, { desc = "[S]earch [/] in Open Files" })
-
-			-- Shortcut for searching your Neovim configuration files
-			vim.keymap.set("n", "<leader>sn", function()
-				builtin.find_files({ cwd = vim.fn.stdpath("config") })
-			end, { desc = "[S]earch [N]eovim files" })
-		end,
-	},
+	-- { -- Fuzzy Finder (files, lsp, etc)
+	-- 	"nvim-telescope/telescope.nvim",
+	-- 	event = "VimEnter",
+	-- 	branch = "0.1.x",
+	-- 	dependencies = {
+	-- 		"nvim-lua/plenary.nvim",
+	-- 		{ -- If encountering errors, see telescope-fzf-native README for installation instructions
+	-- 			"nvim-telescope/telescope-fzf-native.nvim",
+	--
+	-- 			-- `build` is used to run some command when the plugin is installed/updated.
+	-- 			-- This is only run then, not every time Neovim starts up.
+	-- 			build = "make",
+	--
+	-- 			-- `cond` is a condition used to determine whether this plugin should be
+	-- 			-- installed and loaded.
+	-- 			cond = function()
+	-- 				return vim.fn.executable("make") == 1
+	-- 			end,
+	-- 		},
+	-- 		{ "nvim-telescope/telescope-ui-select.nvim" },
+	--
+	-- 		-- Useful for getting pretty icons, but requires a Nerd Font.
+	-- 		{ "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font },
+	-- 	},
+	-- 	config = function()
+	-- 		-- Telescope is a fuzzy finder that comes with a lot of different things that
+	-- 		-- it can fuzzy find! It's more than just a "file finder", it can search
+	-- 		-- many different aspects of Neovim, your workspace, LSP, and more!
+	-- 		--
+	-- 		-- The easiest way to use Telescope, is to start by doing something like:
+	-- 		--  :Telescope help_tags
+	-- 		--
+	-- 		-- After running this command, a window will open up and you're able to
+	-- 		-- type in the prompt window. You'll see a list of `help_tags` options and
+	-- 		-- a corresponding preview of the help.
+	-- 		--
+	-- 		-- Two important keymaps to use while in Telescope are:
+	-- 		--  - Insert mode: <c-/>
+	-- 		--  - Normal mode: ?
+	-- 		--
+	-- 		-- This opens a window that shows you all of the keymaps for the current
+	-- 		-- Telescope picker. This is really useful to discover what Telescope can
+	-- 		-- do as well as how to actually do it!
+	--
+	-- 		-- [[ Configure Telescope ]]
+	-- 		-- See `:help telescope` and `:help telescope.setup()`
+	-- 		require("telescope").setup({
+	-- 			-- You can put your default mappings / updates / etc. in here
+	-- 			--  All the info you're looking for is in `:help telescope.setup()`
+	-- 			--
+	-- 			-- defaults = {
+	-- 			--   mappings = {
+	-- 			--     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
+	-- 			--   },
+	-- 			-- },
+	-- 			-- pickers = {}
+	-- 			extensions = {
+	-- 				["ui-select"] = {
+	-- 					require("telescope.themes").get_dropdown(),
+	-- 				},
+	-- 			},
+	-- 		})
+	--
+	-- 		-- Enable Telescope extensions if they are installed
+	-- 		pcall(require("telescope").load_extension, "fzf")
+	-- 		pcall(require("telescope").load_extension, "ui-select")
+	--
+	-- 		-- See `:help telescope.builtin`
+	-- 		local builtin = require("telescope.builtin")
+	-- 		vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
+	-- 		vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
+	-- 		vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
+	-- 		vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
+	-- 		vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
+	-- 		vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
+	-- 		vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
+	-- 		vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
+	-- 		vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+	-- 		vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
+	--
+	-- 		-- Slightly advanced example of overriding default behavior and theme
+	-- 		vim.keymap.set("n", "<leader>/", function()
+	-- 			-- You can pass additional configuration to Telescope to change the theme, layout, etc.
+	-- 			builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+	-- 				winblend = 10,
+	-- 				previewer = false,
+	-- 			}))
+	-- 		end, { desc = "[/] Fuzzily search in current buffer" })
+	--
+	-- 		-- It's also possible to pass additional configuration options.
+	-- 		--  See `:help telescope.builtin.live_grep()` for information about particular keys
+	-- 		vim.keymap.set("n", "<leader>s/", function()
+	-- 			builtin.live_grep({
+	-- 				grep_open_files = true,
+	-- 				prompt_title = "Live Grep in Open Files",
+	-- 			})
+	-- 		end, { desc = "[S]earch [/] in Open Files" })
+	--
+	-- 		-- Shortcut for searching your Neovim configuration files
+	-- 		vim.keymap.set("n", "<leader>sn", function()
+	-- 			builtin.find_files({ cwd = vim.fn.stdpath("config") })
+	-- 		end, { desc = "[S]earch [N]eovim files" })
+	-- 	end,
+	-- },
 
 	{ -- LSP Configuration & Plugins
 		"neovim/nvim-lspconfig",
@@ -824,7 +721,7 @@ require("lazy").setup({
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
 		opts = {
-			ensure_installed = { "bash", "c", "html", "lua", "markdown", "vim", "vimdoc" },
+			ensure_installed = { "python", "bash", "c", "html", "lua", "markdown", "vim", "vimdoc" },
 			-- Autoinstall languages that are not installed
 			auto_install = true,
 			highlight = {
@@ -920,3 +817,18 @@ vim.keymap.set("n", "<leader>dH", function()
 	gs.diffthis("~")
 end)
 vim.keymap.set("n", "<leader>td", gs.toggle_deleted)
+
+vim.keymap.set("n", "<leader>gg", "<cmd>GrepperRg <C-R><C-W><CR>")
+vim.keymap.set("n", "Q", "<Nop>")
+vim.keymap.set("n", "<C-l>", "<C-i>")
+
+-- opts = { let g:fzf_layout = { 'down':  '40%'} } },
+-- vim.g.fzf_layout = { 'down':  '40%'}
+-- :command! -nargs=1 AG GrepperAg <args>
+-- :command! -nargs=1 RG GrepperRg <args>
+-- iabbrev pdb breakpoint()
+-- let g:fzf_layout = {'down':  '40%'}
+-- let g:airline#extensions#tagbar#enabled = 1
+-- let g:airline#extensions#tagbar#flags = 'f'
+-- ctags
+--
