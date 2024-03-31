20e60f27a7385b4ccd8f84412148ab42bb6004be
noelevans <noelevans@gmail.com>
2024-03-31 22:17:33 +0100

    Lualine with class name
    Also function name and lots of other improvements

--- .config/nvim/work.init.lua
+++ .config/nvim/work.init.lua
@@ -86,24 +86,6 @@ vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagn
 -- or just use <C-\><C-n> to exit terminal mode
 vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 
--- TIP: Disable arrow keys in normal mode
--- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
--- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
--- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
--- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
-
--- Keybinds to make split navigation easier.
---  Use CTRL+<hjkl> to switch between windows
---
---  See `:help wincmd` for a list of all window commands
-vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
-vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
-vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
-vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
-
--- [[ Basic Autocommands ]]
---  See `:help lua-guide-autocommands`
-
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
 --  See `:help vim.highlight.on_yank()`
@@ -124,17 +106,6 @@ if not vim.loop.fs_stat(lazypath) then
 end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
--- [[ Configure and install plugins ]]
---
---  To check the current status of your plugins, run
---    :Lazy
---
---  You can press `?` in this menu for help. Use `:q` to close the window
---
---  To update plugins you can run
---    :Lazy update
---
--- NOTE: Here is where you install your plugins.
 require("lazy").setup({
 	-- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
 	"tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
@@ -191,118 +162,6 @@ require("lazy").setup({
 	-- after the plugin has been loaded:
 	--  config = function() ... end
 
-	-- NOTE: Plugins can specify dependencies.
-	--
-	-- The dependencies are proper plugin specifications as well - anything
-	-- you do for a plugin at the top level, you can do for a dependency.
-	--
-	-- Use the `dependencies` key to specify the dependencies of a particular plugin
-
-	-- { -- Fuzzy Finder (files, lsp, etc)
-	-- 	"nvim-telescope/telescope.nvim",
-	-- 	event = "VimEnter",
-	-- 	branch = "0.1.x",
-	-- 	dependencies = {
-	-- 		"nvim-lua/plenary.nvim",
-	-- 		{ -- If encountering errors, see telescope-fzf-native README for installation instructions
-	-- 			"nvim-telescope/telescope-fzf-native.nvim",
-	--
-	-- 			-- `build` is used to run some command when the plugin is installed/updated.
-	-- 			-- This is only run then, not every time Neovim starts up.
-	-- 			build = "make",
-	--
-	-- 			-- `cond` is a condition used to determine whether this plugin should be
-	-- 			-- installed and loaded.
-	-- 			cond = function()
-	-- 				return vim.fn.executable("make") == 1
-	-- 			end,
-	-- 		},
-	-- 		{ "nvim-telescope/telescope-ui-select.nvim" },
-	--
-	-- 		-- Useful for getting pretty icons, but requires a Nerd Font.
-	-- 		{ "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font },
-	-- 	},
-	-- 	config = function()
-	-- 		-- Telescope is a fuzzy finder that comes with a lot of different things that
-	-- 		-- it can fuzzy find! It's more than just a "file finder", it can search
-	-- 		-- many different aspects of Neovim, your workspace, LSP, and more!
-	-- 		--
-	-- 		-- The easiest way to use Telescope, is to start by doing something like:
-	-- 		--  :Telescope help_tags
-	-- 		--
-	-- 		-- After running this command, a window will open up and you're able to
-	-- 		-- type in the prompt window. You'll see a list of `help_tags` options and
-	-- 		-- a corresponding preview of the help.
-	-- 		--
-	-- 		-- Two important keymaps to use while in Telescope are:
-	-- 		--  - Insert mode: <c-/>
-	-- 		--  - Normal mode: ?
-	-- 		--
-	-- 		-- This opens a window that shows you all of the keymaps for the current
-	-- 		-- Telescope picker. This is really useful to discover what Telescope can
-	-- 		-- do as well as how to actually do it!
-	--
-	-- 		-- [[ Configure Telescope ]]
-	-- 		-- See `:help telescope` and `:help telescope.setup()`
-	-- 		require("telescope").setup({
-	-- 			-- You can put your default mappings / updates / etc. in here
-	-- 			--  All the info you're looking for is in `:help telescope.setup()`
-	-- 			--
-	-- 			-- defaults = {
-	-- 			--   mappings = {
-	-- 			--     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-	-- 			--   },
-	-- 			-- },
-	-- 			-- pickers = {}
-	-- 			extensions = {
-	-- 				["ui-select"] = {
-	-- 					require("telescope.themes").get_dropdown(),
-	-- 				},
-	-- 			},
-	-- 		})
-	--
-	-- 		-- Enable Telescope extensions if they are installed
-	-- 		pcall(require("telescope").load_extension, "fzf")
-	-- 		pcall(require("telescope").load_extension, "ui-select")
-	--
-	-- 		-- See `:help telescope.builtin`
-	-- 		local builtin = require("telescope.builtin")
-	-- 		vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
-	-- 		vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
-	-- 		vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
-	-- 		vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
-	-- 		vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
-	-- 		vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
-	-- 		vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
-	-- 		vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
-	-- 		vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-	-- 		vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
-	--
-	-- 		-- Slightly advanced example of overriding default behavior and theme
-	-- 		vim.keymap.set("n", "<leader>/", function()
-	-- 			-- You can pass additional configuration to Telescope to change the theme, layout, etc.
-	-- 			builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
-	-- 				winblend = 10,
-	-- 				previewer = false,
-	-- 			}))
-	-- 		end, { desc = "[/] Fuzzily search in current buffer" })
-	--
-	-- 		-- It's also possible to pass additional configuration options.
-	-- 		--  See `:help telescope.builtin.live_grep()` for information about particular keys
-	-- 		vim.keymap.set("n", "<leader>s/", function()
-	-- 			builtin.live_grep({
-	-- 				grep_open_files = true,
-	-- 				prompt_title = "Live Grep in Open Files",
-	-- 			})
-	-- 		end, { desc = "[S]earch [/] in Open Files" })
-	--
-	-- 		-- Shortcut for searching your Neovim configuration files
-	-- 		vim.keymap.set("n", "<leader>sn", function()
-	-- 			builtin.find_files({ cwd = vim.fn.stdpath("config") })
-	-- 		end, { desc = "[S]earch [N]eovim files" })
-	-- 	end,
-	-- },
-
 	{ -- LSP Configuration & Plugins
 		"neovim/nvim-lspconfig",
 		dependencies = {
@@ -349,82 +208,82 @@ require("lazy").setup({
 			--    That is to say, every time a new file is opened that is associated with
 			--    an lsp (for example, opening `main.rs` is associated with `rust_analyzer`) this
 			--    function will be executed to configure the current buffer
-			vim.api.nvim_create_autocmd("LspAttach", {
-				group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
-				callback = function(event)
-					-- NOTE: Remember that Lua is a real programming language, and as such it is possible
-					-- to define small helper and utility functions so you don't have to repeat yourself.
-					--
-					-- In this case, we create a function that lets us more easily define mappings specific
-					-- for LSP related items. It sets the mode, buffer and description for us each time.
-					local map = function(keys, func, desc)
-						vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
-					end
-
-					-- Jump to the definition of the word under your cursor.
-					--  This is where a variable was first declared, or where a function is defined, etc.
-					--  To jump back, press <C-t>.
-					map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
-
-					-- Find references for the word under your cursor.
-					map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
-
-					-- Jump to the implementation of the word under your cursor.
-					--  Useful when your language has ways of declaring types without an actual implementation.
-					map("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
-
-					-- Jump to the type of the word under your cursor.
-					--  Useful when you're not sure what type a variable is and you want to see
-					--  the definition of its *type*, not where it was *defined*.
-					map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
-
-					-- Fuzzy find all the symbols in your current document.
-					--  Symbols are things like variables, functions, types, etc.
-					map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
-
-					-- Fuzzy find all the symbols in your current workspace.
-					--  Similar to document symbols, except searches over your entire project.
-					map(
-						"<leader>ws",
-						require("telescope.builtin").lsp_dynamic_workspace_symbols,
-						"[W]orkspace [S]ymbols"
-					)
-
-					-- Rename the variable under your cursor.
-					--  Most Language Servers support renaming across files, etc.
-					map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
-
-					-- Execute a code action, usually your cursor needs to be on top of an error
-					-- or a suggestion from your LSP for this to activate.
-					map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
-
-					-- Opens a popup that displays documentation about the word under your cursor
-					--  See `:help K` for why this keymap.
-					map("K", vim.lsp.buf.hover, "Hover Documentation")
-
-					-- WARN: This is not Goto Definition, this is Goto Declaration.
-					--  For example, in C this would take you to the header.
-					map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
-
-					-- The following two autocommands are used to highlight references of the
-					-- word under your cursor when your cursor rests there for a little while.
-					--    See `:help CursorHold` for information about when this is executed
-					--
-					-- When you move your cursor, the highlights will be cleared (the second autocommand).
-					local client = vim.lsp.get_client_by_id(event.data.client_id)
-					if client and client.server_capabilities.documentHighlightProvider then
-						vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-							buffer = event.buf,
-							callback = vim.lsp.buf.document_highlight,
-						})
-
-						vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
-							buffer = event.buf,
-							callback = vim.lsp.buf.clear_references,
-						})
-					end
-				end,
-			})
+			-- vim.api.nvim_create_autocmd("LspAttach", {
+			-- 	group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
+			-- 	callback = function(event)
+			-- 		-- NOTE: Remember that Lua is a real programming language, and as such it is possible
+			-- 		-- to define small helper and utility functions so you don't have to repeat yourself.
+			-- 		--
+			-- 		-- In this case, we create a function that lets us more easily define mappings specific
+			-- 		-- for LSP related items. It sets the mode, buffer and description for us each time.
+			-- 		local map = function(keys, func, desc)
+			-- 			vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
+			-- 		end
+			--
+			-- 		-- Jump to the definition of the word under your cursor.
+			-- 		--  This is where a variable was first declared, or where a function is defined, etc.
+			-- 		--  To jump back, press <C-t>.
+			-- 		map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
+			--
+			-- 		-- Find references for the word under your cursor.
+			-- 		map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+			--
+			-- 		-- Jump to the implementation of the word under your cursor.
+			-- 		--  Useful when your language has ways of declaring types without an actual implementation.
+			-- 		map("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
+			--
+			-- 		-- Jump to the type of the word under your cursor.
+			-- 		--  Useful when you're not sure what type a variable is and you want to see
+			-- 		--  the definition of its *type*, not where it was *defined*.
+			-- 		map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
+			--
+			-- 		-- Fuzzy find all the symbols in your current document.
+			-- 		--  Symbols are things like variables, functions, types, etc.
+			-- 		map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
+			--
+			-- 		-- Fuzzy find all the symbols in your current workspace.
+			-- 		--  Similar to document symbols, except searches over your entire project.
+			-- 		map(
+			-- 			"<leader>ws",
+			-- 			require("telescope.builtin").lsp_dynamic_workspace_symbols,
+			-- 			"[W]orkspace [S]ymbols"
+			-- 		)
+			--
+			-- 		-- Rename the variable under your cursor.
+			-- 		--  Most Language Servers support renaming across files, etc.
+			-- 		map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
+			--
+			-- 		-- Execute a code action, usually your cursor needs to be on top of an error
+			-- 		-- or a suggestion from your LSP for this to activate.
+			-- 		map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
+			--
+			-- 		-- Opens a popup that displays documentation about the word under your cursor
+			-- 		--  See `:help K` for why this keymap.
+			-- 		map("K", vim.lsp.buf.hover, "Hover Documentation")
+			--
+			-- 		-- WARN: This is not Goto Definition, this is Goto Declaration.
+			-- 		--  For example, in C this would take you to the header.
+			-- 		map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+			--
+			-- 		-- The following two autocommands are used to highlight references of the
+			-- 		-- word under your cursor when your cursor rests there for a little while.
+			-- 		--    See `:help CursorHold` for information about when this is executed
+			-- 		--
+			-- 		-- When you move your cursor, the highlights will be cleared (the second autocommand).
+			-- 		local client = vim.lsp.get_client_by_id(event.data.client_id)
+			-- 		if client and client.server_capabilities.documentHighlightProvider then
+			-- 			vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+			-- 				buffer = event.buf,
+			-- 				callback = vim.lsp.buf.document_highlight,
+			-- 			})
+			--
+			-- 			vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+			-- 				buffer = event.buf,
+			-- 				callback = vim.lsp.buf.clear_references,
+			-- 			})
+			-- 		end
+			-- 	end,
+			-- })
 
 			-- LSP servers and clients are able to communicate to each other what features they support.
 			--  By default, Neovim doesn't support everything that is in the LSP specification.
@@ -660,36 +519,6 @@ require("lazy").setup({
 		opts = { signs = false },
 	},
 
-	{ -- Collection of various small independent plugins/modules
-		"echasnovski/mini.nvim",
-		config = function()
-			-- Better Around/Inside textobjects
-			--
-			-- Examples:
-			--  - va)  - [V]isually select [A]round [)]paren
-			--  - yinq - [Y]ank [I]nside [N]ext [']quote
-			--  - ci'  - [C]hange [I]nside [']quote
-			require("mini.ai").setup({ n_lines = 500 })
-
-			-- Simple and easy statusline.
-			--  You could remove this setup call if you don't like it,
-			--  and try some other statusline plugin
-			local statusline = require("mini.statusline")
-			-- set use_icons to true if you have a Nerd Font
-			statusline.setup({})
-
-			-- You can configure sections in the statusline by overriding their
-			-- default behavior. For example, here we set the section for
-			-- cursor location to LINE:COLUMN
-			---@diagnostic disable-next-line: duplicate-set-field
-			statusline.section_location = function()
-				return "%2l:%-2v"
-			end
-
-			-- ... and there is more!
-			--  Check out: https://github.com/echasnovski/mini.nvim
-		end,
-	},
 	{ -- Highlight, edit, and navigate code
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
@@ -721,44 +550,31 @@ require("lazy").setup({
 		end,
 	},
 
-	-- { "yamatsum/nvim-cursorline", },  -- Try this one next
 	{
-		"nvim-treesitter/nvim-treesitter-refactor",
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
 		config = function()
-			require("nvim-treesitter.configs").setup {
-				refactor = {
-					highlight_definitions = {
-						enable = true,
-						-- Set to false if you have an `updatetime` of ~100.
-						clear_on_cursor_move = true,
+			require("lualine").setup({
+
+				sections = {
+					lualine_c = {
+						"filename",
+						function()
+							return require("nvim-treesitter").statusline({
+								indicator_size = 70,
+								type_patterns = { "class", "function", "method" },
+								separator = " -> ",
+							})
+						end,
 					},
 				},
-			}
+			})
 		end,
 	},
 
-	-- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
-	-- init.lua. If you want these files, they are in the repository, so you can just download them and
-	-- place them in the correct locations.
-
-	-- NOTE: Next step on your Neovim journey: Add/Configure additional plugins for Kickstart
-	--
-	--  Here are some example plugins that I've included in the Kickstart repository.
-	--  Uncomment any of the lines below to enable them (you will need to restart nvim).
-	--
-	-- require 'kickstart.plugins.debug',
-	-- require 'kickstart.plugins.indent_line',
-	-- require 'kickstart.plugins.lint',
-
-	-- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
-	--    This is the easiest way to modularize your config.
-	--
-	--  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-	--    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
-	-- { import = 'custom.plugins' },
+	-- { "yamatsum/nvim-cursorline", },  -- Try this one next
 }, {
-	ui = {
-	},
+	ui = {},
 })
 
 -- The line beneath this is called `modeline`. See `:help modeline`
@@ -794,14 +610,10 @@ vim.keymap.set("n", "<leader>gg", "<cmd>GrepperRg <C-R><C-W><CR>")
 vim.keymap.set("n", "Q", "<Nop>")
 vim.keymap.set("n", "<C-l>", "<C-i>")
 
-vim.g.fzf_layout = {down = "40%"}
+vim.g.fzf_layout = { down = "40%" }
 -- :command! -nargs=1 AG GrepperAg <args>
 -- :command! -nargs=1 RG GrepperRg <args>
 -- iabbrev pdb breakpoint()
--- let g:airline#extensions#tagbar#enabled = 1
--- let g:airline#extensions#tagbar#flags = 'f'
--- ctags
---
 
 vim.keymap.set("ca", "AG", "GrepperAg")
 vim.keymap.set("ca", "RG", "GrepperRg")
